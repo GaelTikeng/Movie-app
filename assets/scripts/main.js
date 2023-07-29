21248efc06df7a961441c4d2e7f4a74be5fcd23d
GaelTikeng <Gaelinho@yahoo.fr>
2023-07-29 23:22:06 +0100

    movies diplayed

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,31 +1,27 @@
-const button = document.querySelector('.btn')
-let displayMovies = document.querySelector('.display')
+const button = document.querySelector('.btn');
+const displayMovies = document.querySelector('.display');
 
-async function fetchData () {
-  const inputSearch = document.querySelector('.input').value
-  const url = `http://www.omdbapi.com/?s=${inputSearch}&apikey=80b18414`
+async function fetchData() {
+  const inputSearch = document.querySelector('.input').value;
+  const url = `http://www.omdbapi.com/?s=${inputSearch}&apikey=80b18414`;
 
   await fetch(url)
-    .then(response => response.json())
-    .then((data) => {
-      console.log(data)
-      let movies = data.Search
-      displayMovies.innerHtml = movies.map((movie, index) => {
+    .then((response) => response.json())
+    .then(({ Search }) => {
+      console.log(Search);
+      displayMovies.innerHTML = Search.map((movie, index) => {
         return `<div class="movie" key=${index}>
-                  <img src="${movie.Poster}" alt="movies" />
-                  <h3 class="title">${movie.Title}</h3>
-                  <h3>${movie.Year}</h3>
-                  <a href="#" class="a-tag">Movie details</a>
-                </div>`;
+            <img src=${movie.Poster} alt="movies" />
+            <h3 class="title">${movie.Title}</h3>
+            <h3>${movie.Year}</h3>
+            <a href="#" class="a-tag">Movie details</a>
+          </div>`;
       });
-      // displayMovies.innerHtml = mappedMovies
-      
-    }) 
-    .catch((err) => {
-      console.log(err)
     })
-  console.log(inputSearch)
-
+    .catch((err) => {
+      console.log(err);
+    });
+  console.log(inputSearch);
 }
 
-button.addEventListener('click', fetchData)
+button.addEventListener('click', fetchData);
