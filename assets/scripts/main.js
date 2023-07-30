57c46b7553043c6af415cedb48d4e3c9e7d0b1e6
GaelTikeng <Gaelinho@yahoo.fr>
2023-07-30 18:42:46 +0100

    display movies on load

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,7 +3,7 @@ const displayMovies = document.querySelector('.display');
 
 async function fetchData() {
   const inputSearch = document.querySelector('.input').value;
-  const url = `http://www.omdbapi.com/?s=${inputSearch}&apikey=80b18414`;
+  const url = `http://www.omdbapi.com/?s=blade&apikey=80b18414`;
 
   await fetch(url)
     .then((response) => response.json())
@@ -14,7 +14,7 @@ async function fetchData() {
             <img src=${movie.Poster} alt="movies" />
             <h3 class="title">${movie.Title}</h3>
             <h3>${movie.Year}</h3>
-            <a href="#" class="a-tag">Movie details</a>
+            <a onclick="selectedMovie('${movie.imdbID}')" href="#" class="a-tag">Movie details</a>
           </div>`;
       });
     })
@@ -23,5 +23,45 @@ async function fetchData() {
     });
   console.log(inputSearch);
 }
-
 button.addEventListener('click', fetchData);
+
+const getMovie = async () => {
+  let movieId = JSON.parse(localStorage.getItem('movieId'))
+  console.log(movieId)
+  const url = `http://www.omdbapi.com/?i=${movieId}&apikey=80b18414`;
+
+  await fetch(url)
+    .then((response) => response.json)
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((err) => console.log("error occured", err))
+
+}
+
+const selectedMovie = (id) => {
+  localStorage.setItem('movieId', JSON.stringify(id))
+  console.log(id)
+}
+
+const displayMovie = async() => {
+  const inputSearch = document.querySelector('.input').value;
+  const url = `http://www.omdbapi.com/?s=blade&apikey=80b18414`;
+
+  await fetch(url)
+    .then((response) => response.json())
+    .then(({ Search }) => {
+      console.log(Search);
+      document.querySelector('.content').innerHTML = Search.map((movie, index) => {
+        return `<div class="movie" key=${index}>
+            <img src=${movie.Poster} alt="movies" />
+            <h3 class="title">${movie.Title}</h3>
+            <h3>${movie.Year}</h3>
+            <a onclick="selectedMovie('${movie.imdbID}')" href="#" class="a-tag">Movie details</a>
+          </div>`;
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
